4edbfafdb203d029c436f20eb622206f7ba2fc8d
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-12 23:53:09 -0800

    Board class complete

--- Lab04_TicTacToe/Classes/Board.cs
+++ Lab04_TicTacToe/Classes/Board.cs
@@ -15,13 +15,16 @@ namespace Lab04_TicTacToe.Classes
 			{"4", "5", "6"},
 			{"7", "8", "9"},
 		};
-
-
 		public void DisplayBoard()
 		{
-			
-			//TODO: Output the board to the console
-		
+            for (int i = 0; i < GameBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < GameBoard.GetLength(1); j++)
+                {
+                    Console.Write($"|{GameBoard[i, j]}|");
+                }
+                Console.WriteLine();
+            }
 		}
 	}
 }
